66fc2a012a2d8d4375d89259bfd9bb814272a1c0
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-12-08 13:08:45 +0530

    feat:Added Swagger For Registration And Login API

--- server.js
+++ server.js
@@ -6,8 +6,12 @@
 */
 const express = require('express');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.urlencoded({ extended: true }));
 
